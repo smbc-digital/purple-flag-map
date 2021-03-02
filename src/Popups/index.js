a4ad11226fe2cf3266f09c34586b28686d95d0ae
stephen.lally <stephen.lally.smbc@gmail.com>
2021-03-02 16:29:47 +0000

    fix(lint): remove unused variable

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,8 +1,6 @@
 import { getTargetUrl } from '../Helpers'
 
 const streetlightPopup = (feature, layer) => {
-  const varName = getTargetUrl()
-
   const content = `<div class="smbc-map__item">
   <div class="smbc-map__item__header__block">
     <i class="fa fa-book smbc-map__item__header__block__icon" aria-hidden="true"></i>
